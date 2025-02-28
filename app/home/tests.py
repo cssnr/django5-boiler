6404a5fd2002d4fae234841ce1a97d2670f82a7b
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-28 15:27:13 -0800

    Add Badge

--- app/home/tests.py
+++ app/home/tests.py
@@ -9,6 +9,7 @@ class TestViews(TestCase):
             "home:news": 200,
             "home:message": 200,
             "home:contact": 200,
+            "oauth:login": 302,
             "api:index": 200,
         }
 
